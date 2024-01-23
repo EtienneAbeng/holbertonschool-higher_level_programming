37e5e6b1560d3b6bf2a646eeb8f7258bea50ef91
EtienneAbeng <abengetienne@gmail.com>
2024-01-23 10:10:38 -0800

    Second push correction 4&5&6

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -6,4 +6,3 @@ middle_word_2 = word[1:8]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word_2}")
-print(f"{word_first_3} {word_last_2} {middle_word_2}")
