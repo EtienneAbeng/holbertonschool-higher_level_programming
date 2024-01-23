61c0dc8436553fa36e09e4e37a9c64d585d418cd
EtienneAbeng <abengetienne@gmail.com>
2024-01-23 09:50:37 -0800

    First commit python

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[7:9]
+middle_word_2 = word[1:8]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word_2}")
+print(f"{word_first_3} {word_last_2} {middle_word_2}")
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[25:52] + str[-23:-17] + str[0:6]
+print(str)
