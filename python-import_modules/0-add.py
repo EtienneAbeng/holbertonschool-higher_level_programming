4f4374292132fd429a5d5d298bb064ab9bfa175d
EtienneAbeng <abengetienne@gmail.com>
2024-01-25 12:22:23 -0800

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result_addition = add_0.add(1, 2)
+    result_addition = add_0.add(1, 2)
 
-print("{:d} + {:d} = {:d}".format(a, b, result_addition))
+    print("{:d} + {:d} = {:d}".format(a, b, result_addition))
