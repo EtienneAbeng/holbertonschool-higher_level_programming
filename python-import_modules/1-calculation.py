a416b076bc845e7ab86fa8cfba028216609c882d
EtienneAbeng <abengetienne@gmail.com>
2024-01-26 10:36:57 -0800

    update

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,18 +2,19 @@
 
 # Import a function from another file
 from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-# Different function used in file imported
-add(a, b)
-sub(a, b)
-mul(a, b)
-div(a, b)
+    # Different function used in file imported
+    add(a, b)
+    sub(a, b)
+    mul(a, b)
+    div(a, b)
 
-# Print the different oepration realized after passage in parameter
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    # Print the different oepration realized after passage in parameter
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
