c5627c78ce213cbec5e3c02e1abd0a898d0b336f
EtienneAbeng <abengetienne@gmail.com>
2024-01-26 10:19:12 -0800

    Update exercice 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Import a function from another file
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+# Different function used in file imported
+add(a, b)
+sub(a, b)
+mul(a, b)
+div(a, b)
+
+# Print the different oepration realized after passage in parameter
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
