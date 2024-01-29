6a6b52bf197b979054d76bbaa768bab00c913d49
EtienneAbeng <abengetienne@gmail.com>
2024-01-29 13:28:27 -0800

    Update ex 1 with while loop and len

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,3 +1,16 @@
+'''
+list of integer - Write a function that prints all integers of a list.
+@str.format : use this format to print the list
+@len : len(ma_liste) pour déterminer la condition de fin de la boucle
+'''
+
+
 def print_list_integer(my_list=[]):
-    while my_list:
-        print("{0}".format(my_list))
+
+    # Initialize the counter index to browse the list
+    i = 0
+    # Browse the list with i to print the list
+    while i < len(my_list):
+        print("{}".format(my_list[i]))
+        # Each end of iteration to add + 1 at the counter
+        i += 1
