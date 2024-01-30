d79304407aa123ecb099aea92f921f014168e405
EtienneAbeng <abengetienne@gmail.com>
2024-01-30 02:01:36 -0800

    Update

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,13 +2,11 @@
 
 def element_at(my_list, idx):
 
-    # Use a loop with range to browse the index of list
-    for idx in my_list range(0, 4):
         # If number to compare index is negatif, the list don't read
         if idx < 0:
             return None
-        # If number to compare index is high , the list also don't read
-        elif idx > 5:
+
+        elif idx > len(my_list):
             return None
         # Also print the number which find at the good index
         else:
