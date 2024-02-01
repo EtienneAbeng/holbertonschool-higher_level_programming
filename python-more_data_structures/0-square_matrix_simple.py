6f4dfb40fdd807293341574014fea3ea428e15c3
EtienneAbeng <abengetienne@gmail.com>
2024-02-01 06:19:05 -0800

    Update because comment is too long

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,13 @@
 def square_matrix_simple(matrix=[]):
-    # Création d'une nouvelle matrice avec les mêmes dimensions que la matrice d'origine
+    # Création d'une nouvelle matrice avec les mêmes dimensions que l'origine
     new_matrix = []
-    
+
     # Parcours des lignes de la matrice d'origine
     for row in matrix:
-        new_row = []  # Nouvelle ligne pour la nouvelle matrice
+        new_row = []  # Nouvelle ligne pour la new matrice
         # Parcours des éléments de chaque ligne
         for element in row:
-            new_row.append(element**2)  # Ajout du carré de l'élément à la nouvelle ligne
-        new_matrix.append(new_row)  # Ajout de la nouvelle ligne à la nouvelle matrice
-    
+            new_row.append(element**2)  # Ajout du carré à la nouvelle ligne
+        new_matrix.append(new_row)  # Ajout nouvelle ligne à la new matrice
+
     return new_matrix
