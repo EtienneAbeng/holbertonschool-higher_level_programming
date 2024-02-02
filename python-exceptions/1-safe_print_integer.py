3ee1db2966c76487ef136ba415fde266a07f2ff2
EtienneAbeng <abengetienne@gmail.com>
2024-02-02 14:57:08 -0800

    update

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))  # Essaye d'imprimer la valeur comme un entier
-        return True  # Retourne True si l'impression réussit
+        print("{:d}".format(value))
+        return (True)
     except (TypeError, ValueError):
-        return False  # Retourne False en cas d'erreur de type ou de valeur
+        return (False)
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
     try:
-        # Initialise un compteur pour le nombre d'entiers imprimés
-        count = 0
-        # Boucle à travers les éléments de my_list jusqu'à x
         for i in range(x):
-            # Vérifie si l'élément est un entier
-            if isinstance(my_list[i], int):
-                # Utilise "{:d}".format() pour imprimer l'entier
+            if type(my_list[i]) == int:
                 print("{:d}".format(my_list[i]), end="")
-                # Incrémente le compteur
                 count += 1
-        # Imprime une nouvelle ligne
+    except IndexError:
+        pass
+    finally:
         print()
-        # Retourne le nombre d'entiers imprimés
         return count
-    except (ValueError, TypeError, IndexError):
-        # En cas d'erreur (ValueError, TypeError ou IndexError)
-        return 0
