5ec09c7d120459ea9dc9e1a9dd8d8fe7d0c95f7d
EtienneAbeng <abengetienne@gmail.com>
2024-02-02 14:37:45 -0800

    update

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    # Utilise une expression de dictionnaire afin créer un nouveau dictionnaire
+    # où chaque valeur est multipliée par 2
+    new_dictionary = {key: value * 2 for key, value in a_dictionary.items()}
+    return new_dictionary  # Retourne le nouveau dictionnaire
 
-def multiply_by_2(a_dictionary)
-
-    # Vérifie si le dictionnaire est None
-    if a_dictionary is None:
-        return None
-
-    # Crée une copie du dictionnaire
-    new_dict = a_dictionary.copy()
-
-    # Parcourt chaque clé du dictionnaire
-    for key in new_dict:
-        # Multiplie la valeur de chaque clé par 2
-        new_dict[key] *= 2
-
-    # Retourne le nouveau dictionnaire modifié
-    return new_dict
