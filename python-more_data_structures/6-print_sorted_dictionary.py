dd89fd4e260d72f6f685587d5d0dafccbadd1bd4
EtienneAbeng <abengetienne@gmail.com>
2024-02-01 22:38:48 -0800

    update tabulation

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    # Vérifier si le dictionnaire est None
+    if a_dictionary is None:
+        return None
+
+    # Utiliser la fonction sorted() pour obtenir une liste triée des clés
+    sorted_keys = sorted(a_dictionary.keys())
+
+    # Parcourir les clés triées et afficher chaque paire clé-valeur
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        print("{}: {}".format(key, value))
+
+# Exemple d'utilisation
+my_dict = {'language': "Python", 'number': 42, 'track': "High level"}
+print_sorted_dictionary(my_dict)
+
