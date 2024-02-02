89c3efa71c2eb0ec2bb39b4b90228db043f8e333
EtienneAbeng <abengetienne@gmail.com>
2024-02-02 13:39:48 -0800

    first commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    # Vérifie si l'indice (idx) est dans la plage valide
     if idx >= 0 and idx < len(my_list):
+        # Supprime l'élément à l'indice donné
         del my_list[idx]
+    
+    # Retourne la liste mise à jour
     return my_list
