dddcf9908ae9fd0a521ffafa8782c6c6d7885364
EtienneAbeng <abengetienne@gmail.com>
2024-02-01 22:01:23 -0800

    First commit

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+
+    # Vérifier si l'un des ensembles est None
+    if set_1 is None or set_2 is None:
+        return None
+
+    # Utiliser l'opérateur ^ pour obtenir l'ensemble des éléments présents dans un seul ensemble
+    return set_1 ^ set_2
---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+
+    # Vérifier si le dictionnaire est None
+    if a_dictionary is None:
+        return 0
+
+    # Utiliser la fonction len() pour obtenir le nombre de clés dans le dictionnaire
+    nb_keys = len(a_dictionary.keys())
+    return nb_keys
