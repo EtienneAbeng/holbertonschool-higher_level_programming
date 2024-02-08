ce7c27865827e8d0164297a5bbb14a85b2e710eb
EtienneAbeng <abengetienne@gmail.com>
2024-02-08 03:27:43 -0800

    update correction

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,6 +5,7 @@ Attribut d'instance privée : taille
 Instanciation avec taille (pas de vérification de type/valeur)
 '''
 
+
 class Square:
     """Définit une classe Square."""
 
@@ -15,4 +16,3 @@ class Square:
             size (int): La taille du carré.
         """
         self.__size = size  # Attribut privé pour stocker la taille du carré
-
