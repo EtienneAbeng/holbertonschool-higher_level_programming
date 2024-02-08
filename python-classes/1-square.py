43bfbb6d1199461f8402a2cd2e6025f5bbd048f3
EtienneAbeng <abengetienne@gmail.com>
2024-02-08 03:58:02 -0800

    Update comment

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-'''Écrivez une classe Square qui définit un carré
+'''
+Ce script définit une classe Square qui représente un carré.
+La classe a un attribut privé __size qui représente la taille du côté du carré.
+Lors de l'initialisation de la classe, la taille du carré est définie.
+Si la taille n'est pas spécifiée, elle est définie par défaut à 0.
 
-Attribut d'instance privée : taille
-Instanciation avec taille (pas de vérification de type/valeur)
+La classe a une vérification pour s'assurer que la taille est un entier positif
+lors de l'initialisation.
+Si la taille n'est pas un entier ou si négative, une exception est levée.
 '''
 
 
 class Square:
-    """Définit une classe Square."""
+    '''Définit une classe Square.'''
 
     def __init__(self, size):
-        """Initialise un nouveau carré.
+        '''Initialise un nouveau carré.
 
         Args:
             size (int): La taille du carré.
-        """
+        '''
         self.__size = size  # Attribut privé pour stocker la taille du carré
