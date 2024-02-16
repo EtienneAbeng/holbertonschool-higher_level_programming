6a0c7c66d466381e6e792b10668cff6b866cd2ef
EtienneAbeng <abengetienne@gmail.com>
2024-02-15 22:08:02 -0800

    Correction code

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,13 +9,6 @@ mais trier dans croissant en tri ascendant
 class MyList(list):
     """Une sous-classe de list avec une méthode supplémentaire"""
 
-    def append(self, element):
-        """Ajoute un élément à la liste s'il s'agit d'un entier"""
-
-        if not isinstance(element, int):
-            raise TypeError("The value must an integer")
-        super().append(element)
-
     def print_sorted(self):
         """Imprime la liste triée"""
 
