0643b081f87cf20cb46a272d7c5241e708bdc4b4
EtienneAbeng <abengetienne@gmail.com>
2024-02-23 18:40:30 +0100

    update raise

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,7 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
 
         elif value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("x must be > 0")
         else:
             self.__x = value
 
@@ -92,7 +92,7 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
 
         elif value < 0:
-            raise ValueError("y must be >= 0")
+            raise ValueError("y must be > 0")
 
         else:
             self.__y = value
