1f0eb56aeeffdf8a74e31d78ca8a66c05692de74
EtienneAbeng <abengetienne@gmail.com>
2024-03-19 16:47:01 +0100

    delete hbn_0_n

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -2,7 +2,7 @@
 SELECT score, name
 
 -- <FROM> Extraire la base de données "hbtn_0c_0" à de la seconde colonne
-FROM hbtn_0c_0.second_table
+FROM second_table
 
 -- <ORDER BY> Trie les résultats par score, <DESC> permet en ordre décroissant.
 ORDER BY score DESC;
