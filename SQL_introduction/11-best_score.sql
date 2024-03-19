2cea72430392af57a318344a5a2c5d3d2e4246a2
EtienneAbeng <abengetienne@gmail.com>
2024-03-19 16:51:06 +0100

    delete hbn_0_n

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -2,7 +2,7 @@
 SELECT score, name
 
 -- <FROM> Extrait les données de la table "second_table" de la base de données "hbtn_0c_0"
-FROM hbtn_0c_0.second_table
+FROM second_table
 
 -- <WHERE> Filtre les enregistrements pour inclure uniquement ceux avec un score >= 10
 WHERE score >= 10
