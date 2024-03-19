9965128055f24d7c4e3119f10e6454e7088a1e06
EtienneAbeng <abengetienne@gmail.com>
2024-03-19 16:18:55 +0100

    delete hbn_0_n

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- <SELECT> <COUNT> liste et compte le nombre d'entree et <WHERE> filtre seulement id = 89
 
-SELECT COUNT(id) FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
