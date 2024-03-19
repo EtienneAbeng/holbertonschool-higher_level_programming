472e3efa031a5e24a760561d1629fb309dd50e25
EtienneAbeng <abengetienne@gmail.com>
2024-03-19 17:32:01 +0100

    a script that removes all records with a score <= 5 in the table

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- <DELETE> Supprime tous les enregistrements avec un score <= 5 de la table second_table dans la base de données hbtn_0c_0
+DELETE FROM second_table
+
+-- <WHERE> Filtre les enregistrements basés sur un score inférieur ou égal à 5
+WHERE score <= 5;
