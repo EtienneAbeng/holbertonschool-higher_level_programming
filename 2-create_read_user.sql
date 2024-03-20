80741fafb07ea1fd8b6c222a2eca54b9843bb095
EtienneAbeng <abengetienne@gmail.com>
2024-03-20 14:32:46 +0100

    Update old code and chand GRANT ALL for GRANT SELECT

--- 2-create_read_user.sql
+++ 2-create_read_user.sql
@@ -1,13 +1,14 @@
--- <CREATE DATABASE> Creer la base de donnee hbtn_0d_2 si elle existe pas deja
+-- <CREATE DATABASE> Créer la base de données hbtn_0d_2 si elle n'existe pas déjà
 CREATE DATABASE 
     IF NOT EXISTS hbtn_0d_2;
 
--- <CREATE USER> crée un nouvel utilisateur s'il n'existe pas déjà, sur le localhost
--- <IDENTIFIED BY> définit un password pour le nouvel utilisateur
+-- <CREATE USER> Crée un nouvel utilisateur s'il n'existe pas déjà, sur le localhost
+-- <IDENTIFIED BY> Définit un mot de passe pour le nouvel utilisateur
 CREATE USER 
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT ALL PRIVILEGES
-    ON *.*
+-- Accorder le privilège SELECT sur toutes les tables dans la base de données hbtn_0d_2 à l'utilisateur user_0d_2
+GRANT SELECT
+    ON hbtn_0d_2.*  -- Accorde le privilège SELECT sur toutes les tables dans la base de données hbtn_0d_2
     TO 'user_0d_2'@'localhost';
