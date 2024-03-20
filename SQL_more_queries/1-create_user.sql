f7a96eac6f82f56b76dd9c1c05d8b01f879414a1
EtienneAbeng <abengetienne@gmail.com>
2024-03-20 11:52:55 +0100

    Update and structure my code for better listining

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- <CREATE> <USER> permet de creer un nouveau utilisateur dans mon locale host
 -- <IDENTIFIED BY> permet de creer un mot de password pour le nouveau <USER>
-CREATE USER IF NOT EXISTS 'user_0d_1' @ 'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'@'localhost' 
+    IDENTIFIED BY 'user_0d_1_pwd';
