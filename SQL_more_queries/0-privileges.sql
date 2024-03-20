bc9a2319fd8eb0183cef0cee349a48b9f793f9e4
EtienneAbeng <abengetienne@gmail.com>
2024-03-20 11:05:21 +0100

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Active: 1710860491090@@127.0.0.1@3306
+-- <GRANTS> permet de donner des autorisations, donc ce scrpit Liste tous les privilèges des utilisateurs user_0d_1 et user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
