2124422b83ca9fdc92aca83b6d354c8d5513ff39
EtienneAbeng <abengetienne@gmail.com>
2024-03-24 12:51:36 +0100

    Filter states by user input

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Ce script liste tous les états de la base de données hbtn_0e_0_usa."""
+"""Ce script liste tous les états de la base de données hbtn_0e_0_usa dont le nom commence par 'N' (upper N)."""
 
 import MySQLdb  # Importation du module MySQLdb pour interagir avec la base de données
 import sys  # Importation du module sys pour accéder aux arguments de ligne de commande
@@ -22,8 +22,10 @@ if __name__ == "__main__":
     # Création d'un curseur pour exécuter des requêtes SQL
     cursor = db.cursor()
 
-    # Exécution de la requête SQL pour obtenir tous les états
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    # Exécution de la requête SQL pour obtenir tous les états commençant par 'N'
+    # LIKE est utilisé pour rechercher des correspondances partielles dans les chaînes de caractères
+    # Le '%' est un caractère générique qui représente zéro ou plusieurs caractères.
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
 
     # Récupération de tous les résultats
     results = cursor.fetchall()
