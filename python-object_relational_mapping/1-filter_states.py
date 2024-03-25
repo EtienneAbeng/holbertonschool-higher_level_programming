f7aa4c4b4ea3b04ea31b8cf42362085549643189
EtienneAbeng <abengetienne@gmail.com>
2024-03-25 11:51:14 +0100

    add my old code to check if works

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-"""
-script that lists all states with a name
-starting with N (upper N) from the database
-hbtn_0e_0_usa:
-"""
-import MySQLdb
-from sys import argv
+"""Ce script liste tous les états de la base de données hbtn_0e_0_usa dont le nom commence par 'N' (upper N)."""
 
-if __name__ == '__main__':
-    """
-    Main execution block
-    Connects to the database and retrieves the states
-    """
-    # Connect to the MySQL database using the provided arguments
+import MySQLdb  # Importation du module MySQLdb pour interagir avec la base de données
+import sys  # Importation du module sys pour accéder aux arguments de ligne de commande
+
+if __name__ == "__main__":
+    # Paramètres de connexion à la base de données
+    username = sys.argv[1]  # Nom d'utilisateur MySQL
+    password = sys.argv[2]  # Mot de passe MySQL
+    database = sys.argv[3]  # Nom de la base de données MySQL
+
+    # Connexion au serveur MySQL
     db = MySQLdb.connect(
-        host="localhost",  # Hostname of the database server
-        user=argv[1],     # Username obtained from command-line argument
-        port=3306,        # Port number of the database server
-        passwd=argv[2],   # Password obtained from command-line argument
-        db=argv[3]
-    )       # Database name obtained from command-line argument
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    # Create a cursor object to execute SQL queries
-    cur = db.cursor()
+    # Création d'un curseur pour exécuter des requêtes SQL
+    cursor = db.cursor()
 
-    # Execute an SQL query to select rows with names starting with 'N'
-    # from the 'states' table and sort by states.id
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY\
-            'N%' ORDER BY states.id ASC")
+    # Exécution de la requête SQL pour obtenir tous les états commençant par 'N'
+    # LIKE est utilisé pour rechercher des correspondances partielles dans les chaînes de caractères
+    # Le '%' est un caractère générique qui représente zéro ou plusieurs caractères.
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
-    # Fetch all rows returned by the query
-    rows = cur.fetchall()
+    # Récupération de tous les résultats
+    results = cursor.fetchall()
 
-    # Iterate through the fetched rows and print each row
-    for row in rows:
+    # Affichage des résultats
+    for row in results:
         print(row)
+
+    # Fermeture du curseur et de la connexion à la base de données
+    # cursor.close()
+    # db.close()
