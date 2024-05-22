ea1e8a18a5d0d17784103bd387eeb6dcda3bfb67
EtienneAbeng <abengetienne@gmail.com>
2024-05-22 20:03:02 +0200

    Write a JavaScript script that updates the text color of the header element to red (#FF0000)

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Utiliser la méthode document.querySelector() pour récupérer l'élément HTML <header>.
+const header = document.querySelector('header');
+
+// Mettre en rouge le texte du header.
+header.style.color = "red";
