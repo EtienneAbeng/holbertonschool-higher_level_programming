c8122897d621d622db341dee4987a630be8f7a63
EtienneAbeng <abengetienne@gmail.com>
2024-05-23 07:16:08 +0200

    Write a JavaScript script that updates the text of the header element to New Header!!! when the user clicks on the element with id update_header

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,9 @@
+// Sélectionne l'élément avec l'ID 'update_header'
+const updateHeader = documentquer.querySelector('#update_header');
+// Ajoute un gestionnaire d'événements pour réagir au clic sur l'élément update_header
+updateHeader.addEventListener('click', function () {
+  // Sélectionne l'élément <header>
+  const header = document.querySelector('header');
+  // Met à jour le texte de l'élément <header>
+  header.textContent = 'New Header!!!';
+});
