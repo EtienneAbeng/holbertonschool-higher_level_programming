b0e7af9469700bbfb99f9641cf3044e06b8fb973
EtienneAbeng <abengetienne@gmail.com>
2024-05-28 09:48:22 +0200

    update code

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -3,9 +3,12 @@ const redHeader = document.querySelector('#red_header');
 
 // Ajoute un gestionnaire d'événements pour réagir au clic sur l'élément red_header
 redHeader.addEventListener('click', function () {
-  // Sélectionne l'élément <header>
+  // Sélectionne l'élément <header> s'il existe
   const header = document.querySelector('header');
-
-  // Ajoute la classe 'red' à l'élément <header>
-  header.classList.add('red');
+  
+  // Vérifie si l'élément <header> existe et que la classe 'red' n'est pas déjà présente
+  if (header && !header.classList.contains('red')) {
+    // Ajoute la classe 'red' à l'élément <header>
+    header.classList.add('red');
+  }
 });
