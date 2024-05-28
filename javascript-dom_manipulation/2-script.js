751b218b6176443ab75c8bda665f364fa9675abb
EtienneAbeng <abengetienne@gmail.com>
2024-05-28 09:46:08 +0200

    change const header taht select 'header'

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -4,7 +4,7 @@ const redHeader = document.querySelector('#red_header');
 // Ajoute un gestionnaire d'événements pour réagir au clic sur l'élément red_header
 redHeader.addEventListener('click', function () {
   // Sélectionne l'élément <header>
-  const header = document.querySelector('#red_header');
+  const header = document.querySelector('header');
 
   // Ajoute la classe 'red' à l'élément <header>
   header.classList.add('red');
