2ef3392664a0c47d7b3c40e94682b8e1bc631694
EtienneAbeng <abengetienne@gmail.com>
2024-05-28 09:44:36 +0200

    change const header

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -4,7 +4,7 @@ const redHeader = document.querySelector('#red_header');
 // Ajoute un gestionnaire d'événements pour réagir au clic sur l'élément red_header
 redHeader.addEventListener('click', function () {
   // Sélectionne l'élément <header>
-  const header = document.querySelector('#red_header');
+  const header = document.querySelector('header');
 
   // Change la couleur du texte de l'élément <header> en rouge (#FF0000)
   header.style.color = '#FF0000';
